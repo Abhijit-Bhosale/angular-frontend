8257169fedb300fdf4cd0dac51a98f701557247d
Abhijit-Bhosale <abhirajebhosale3972@gmail.com>
2021-01-19 16:01:42 +0530

    Abhijit-front_Back Book

---
+++ src/app/book.service.ts
@@ -0,0 +1,30 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from './book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private baseURL = "http://localhost:8080/api/v1/books"
+  constructor(private httpClient: HttpClient) { }
+
+  getBookList(): Observable<Book[]> {
+    return this.httpClient.get<Book[]>(`${this.baseURL}`);
+  }
+
+  createBook(book: Book): Observable<Object> {
+    return this.httpClient.post(`${this.baseURL}`, book);
+  }
+  getBookByIsbn(isbn: String): Observable<Book> {
+    return this.httpClient.get<Book>(`${this.baseURL}/${isbn}`);
+  }
+  updateBook(isbn: String, book: Book): Observable<Object> {
+    return this.httpClient.put(`${this.baseURL}/${isbn}`, book);
+  }
+  deleteBook(BookId: number): Observable<object> {
+    return this.httpClient.delete(`${this.baseURL}/${BookId}`);
+  }
+}
